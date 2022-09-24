6cbaa747efd80bb479b5d1797ce5f649dce9b733
tarun1git <tarunrajput7959@gmail.com>
2022-09-25 03:44:48 +0530

    animation update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const TimeLine = gsap.timeline({default: {ease: 'power2.out'}});
 
-TimeLine.to(".slider", {y: "100%", duration: 1.2});
+TimeLine.to(".slider", {y: "-100%", duration: 1.2});
+TimeLine.to(".intro .hero", {y: "0%", duration: 1.2}, "-=1");
+TimeLine.fromTo(".nav", {opacity: 0}, {opacity:1, duration: 1});
